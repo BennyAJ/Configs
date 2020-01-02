9250e356499db59cc23a7e02108c5b8472997c5d
Benny Johnson <bennyaj@umich.edu>
2020-01-02 00:54:24 -0500

    More updates to awesome

---
+++ home/awesome/.config/awesome/configuration/apps.lua
@@ -0,0 +1,24 @@
+local filesystem = require('gears.filesystem')
+local with_dpi = require('beautiful').xresources.apply_dpi
+local get_dpi = require('beautiful').xresources.get_dpi
+
+return {
+  -- List of apps to start by default on some actions
+  default = {
+    terminal = 'alacritty',
+    editor = 'code',
+    rofi = 'rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi',
+    lock = 'i3lock-fancy-rapid 5 3 -k --timecolor=ffffffff --datecolor=ffffffff',
+    quake = 'alacritty --title QuakeTerminal'
+  },
+  -- List of apps to start once on start-up
+  run_on_start_up = {
+    'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
+    'blueman-tray', -- Bluetooth tray icon
+    'xfce4-power-manager', -- Power manager
+    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    'nm-applet', -- Wifi
+    'pasystray', -- PulseAudio Tray Icon
+    'cbatticon -x xfce4-power-manager-settings' -- Battery Icon
+  }
+}
